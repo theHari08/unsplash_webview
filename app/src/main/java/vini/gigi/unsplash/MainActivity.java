4656bae067f636f82261015effe2dd1ee481828f
Vinesh Iyappan <ivinesh1997@gmail.com>
2019-11-12 15:25:15 +0530

    cleanup and add swipe to refresh
    Signed-off-by: theHari08 <ivinesh1997@gmail.com>

--- app/src/main/java/vini/gigi/unsplash/MainActivity.java
+++ app/src/main/java/vini/gigi/unsplash/MainActivity.java
@@ -2,31 +2,50 @@ package vini.gigi.unsplash;
 
 import android.Manifest;
 import android.app.DownloadManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Environment;
+import android.view.View;
 import android.webkit.DownloadListener;
 import android.webkit.URLUtil;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mywebview;
+    private ProgressBar myprogressbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mywebview = (WebView)findViewById(R.id.webView);
-        WebSettings webSettings = mywebview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
         mywebview.loadUrl("https://unsplash.com");
         mywebview.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        final SwipeRefreshLayout myswiperefresh;
+        myswiperefresh = findViewById(R.id.swiperefresh);
 
         requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},5);
+
+        //SwipeRefreshLayout
+        myswiperefresh.setOnRefreshListener( new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // This method performs the actual data-refresh operation.
+                // The method calls setRefreshing(false) when it's finished.
+                mywebview.loadUrl(mywebview.getUrl());
+            }
+        });
 
         mywebview.setDownloadListener(new DownloadListener() {
             public void onDownloadStart(String url, String userAgent,
@@ -44,6 +63,33 @@ public class MainActivity extends AppCompatActivity {
                 Toast.LENGTH_LONG).show();
             }
         });
+
+        // Get the widgets reference from XML layout
+
+        myprogressbar = findViewById(R.id.pb);
+        mywebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                // Visible the progressbar
+                myprogressbar.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                myswiperefresh.setRefreshing(false);
+                myprogressbar.setVisibility(View.GONE);
+            }
+        });
+
+        mywebview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int newProgress){
+                // Update the progress bar with page loading progress
+                myprogressbar.setProgress(newProgress);
+                if(newProgress == 100){
+                    // Hide the progressbar
+                    myprogressbar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     @Override
@@ -55,6 +101,4 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-
-
 }
